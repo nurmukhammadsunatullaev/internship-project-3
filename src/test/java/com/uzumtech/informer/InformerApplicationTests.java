084528583eb10cba714d59ed7b45225fb67f6063
djam388 <djam388@hotmail.com>
2024-09-25 17:19:18 +0500

    implement docker + kafka

---
+++ src/test/java/com/uzumtech/informer/InformerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uzumtech.informer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
